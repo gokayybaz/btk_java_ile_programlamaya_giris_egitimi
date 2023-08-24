4566376e542fc60b5c1e3ed4d5949506078252bc
gokayybaz <gokaybaz2000@gmail.com>
2023-08-24 11:32:05 +0300

    Ders 1 : Hello World !

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Hello World Programı
+        String hello = "Hello World !";
+        String merhaba = "Merhaba Java !";
+        System.out.println(hello);
+        System.out.println(merhaba);
+    }
+}
